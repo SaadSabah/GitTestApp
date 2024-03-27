ea854c42f0daf805e0328c710b4ac9d9fbe9e523
SaadSabah <144815057+SaadSabah@users.noreply.github.com>
2024-03-27 11:42:27 +0100

    Add project files.

---
+++ GitTestApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//this line is the first code of my project!
+Console.WriteLine("This is first line!");
+Console.ReadLine();
